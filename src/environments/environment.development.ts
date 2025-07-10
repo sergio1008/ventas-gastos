75df7542621133e534d197e2f037e8fe37b11da9
sergio1008 <sergiobernalc@outlook.com>
2025-07-10 10:31:47 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
     firebase:{
-        apiKey: "AIzaSyACP0wJU36dil9MTox5F75MRBa0wmmT24o",
+        apiKey: "",
         authDomain: "gessas-pos.firebaseapp.com",
-        databaseURL: "https://gessas-pos-default-rtdb.firebaseio.com",
+        databaseURL: "",
         projectId: "gessas-pos",
         storageBucket: "gessas-pos.firebasestorage.app",
         messagingSenderId: "79482947381",
-        appId: "1:79482947381:web:63d2bef6a0f50064502e7c",
+        appId: "",
         useEmulators: true,
       }
 };
